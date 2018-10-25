62f904df8990e55e2ea1ca565fd545b3a564fdf6
KoheiKubo <chococo1423@yahoo.co.jp>
2018-10-25 14:09:37 +0900

    userscreen,question-list,renameを作成しました。

---
+++ src/app/http/question-list/question-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-question-list',
+  templateUrl: './question-list.component.html',
+  styles: ['']
+})
+export class QuestionListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+  }
+}
---
+++ src/app/http/rename/rename.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rename',
+  templateUrl: './rename.component.html',
+  styleUrls: ['./rename.component.css']
+})
+export class RenameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
